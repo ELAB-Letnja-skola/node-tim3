6268ea1b7030206a664d2024ae21f368b56f3428
majapavlovic <maja.pavlovic2703@gmail.com>
2021-10-14 20:27:39 +0200

    Drugi request

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const port = process.env.PORT || 4000;
 
 // TODO - Write a get request using path "/" that will return response with the status code 200 and json message 'This is a first request.'"
 
+
 // TODO - Write a get request using path "/second" that will return response with the status code 200 and json message 'This is a second request.'"
+app.get("/second", (req, res )=> {
+  res.status(200).json("This is a second request.");
+});
 
 // TODO - Write a get request using path "/third" that will return response with the status code 200 and json message 'This is a third request.'"
+
 
 module.exports = app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
